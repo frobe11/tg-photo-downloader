4b7712c9cd552187fb4422a1db27378af287adee
frobe11 <frobe11@mail.ru>
2025-04-22 16:32:27 +0300

    final fix(work local)

--- fetchImages.js
+++ fetchImages.js
@@ -1,6 +1,5 @@
-import { message } from "telegram/client/index.js";
-import { Api } from "../tl";
-
+import { Api } from "telegram/tl/index.js";
+import path from "path";
 
 // sort -1 : first posts with least reactions amount
 // sort 1 : first posts with the most reactions amount
@@ -15,12 +14,12 @@ export default async function fetchImages(
   port
 ) {
   const result = [];
-  const tempResult = [];
   const channel = await client.getEntity(channelTag);
   const messages = await client.getMessages(channel, {
     limit: 10000,
     filter: new Api.InputMessagesFilterPhotos(),
   });
+
   if (end > messages.length) {
     console.error(
       `end: ${end} is greater than messages length: ${messages.length}`
@@ -30,11 +29,23 @@ export default async function fetchImages(
     );
   }
 
-  tempResult = parseMessages(start, end, messages);
+  // Parse messages and get the parsed result
+  const tempResult = parseMessages(
+    start,
+    end,
+    messages,
+    channelTag,
+    domain,
+    port
+  );
+
+  // Sort the parsed result if required
   if (sort) {
     tempResult.sort((first, second) => sort * (second.rating - first.rating));
   }
-  for (let i = start; i < end; i++) {
+
+  // Process the parsed result
+  for (let i = 0; i < tempResult.length; i++) {
     result.push({
       filePath: tempResult[i].filePath,
       fileUrl: tempResult[i].fileUrl,
@@ -57,22 +68,27 @@ async function downloadImage(client, file, filePath) {
   }
 }
 
-async function parseMessages(start, end, messages) {
+function parseMessages(start, end, messages, channelTag, domain, port) {
+  const result = [];
   for (let i = start; i < end; i++) {
     let message = messages[i];
     let rating = 0;
     const file = message.photo;
     const caption = message.message;
+
     if (!message.reactions) {
       console.log(`No reactions for message: ${message.message}`);
     }
+
     if (message.reactions && message.reactions.results) {
-      message.reactions.results.forEach((result) => {
-        rating += result.count;
+      message.reactions.results.forEach((reaction) => {
+        rating += reaction.count;
       });
     }
-    const filePath = `images\\${channelTag}_${message.id}.jpg`;
+
+    const filePath = path.join("images", `${channelTag}_${message.id}.jpg`);
     const fileUrl = `${domain}:${port}/images/${channelTag}_${message.id}.jpg`;
+
     result.push({
       file: file,
       filePath: filePath,
@@ -82,4 +98,7 @@ async function parseMessages(start, end, messages) {
       id: message.id,
     });
   }
+
+  // Return the parsed result
+  return result;
 }
